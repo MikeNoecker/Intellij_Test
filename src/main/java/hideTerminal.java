09a92e291bcdeeff885564f8830ea9907b99ae82
MikeNoecker <michaeldn27@gmail.com>
2022-08-06 12:01:26 -0400

    Initial project files for BrutusJE

---
+++ src/main/java/hideTerminal.java
@@ -0,0 +1,11 @@
+import java.net.NetPermission;
+import java.net.NetworkInterface;
+import java.security.*;
+public class hideTerminal {
+
+    public static void main(String[] args) throws Exception {
+        
+
+        //System.out.println("Updating... *wink*");
+    }
+}
